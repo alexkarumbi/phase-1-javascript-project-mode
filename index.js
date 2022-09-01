00d2bc017ec2f720b1a687d9a2f9889b27b1d664
AlexKarumbi <alex.muhia@moringaschool.com>
2022-09-02 01:11:07 +0300

    Add post method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let objGlobal;
+let counter =0;
 const quotesOutput=document.querySelector("#quotes-container");
 const authorDisplay =document.querySelector("#author-container");
 const sharedBy=document.querySelector("#sharedby");
@@ -6,7 +7,7 @@ const likesDisplay=document.querySelector("#likes");
 const dislikesDisplay=document.querySelector("#dislikes");
 const addLike =document.querySelector("#like-btn");
 const addDislike =document.querySelector("#dislike-btn");
-const addQuote=document.querySelector("#addquote-btn")
+const addQuote=document.querySelector("#addquote-btn");
 
 
 
@@ -36,8 +37,11 @@ function renderList(characterObj){
         li.textContent=element.Quote;
         quotesOutput.append(li);
 
-        
-        authorDisplay.textContent=element.author;
+        const li2 =document.createElement("li2");
+        li2.textContent=element.author
+        authorDisplay.append(li2);
+
+
         likesDisplay.textContent=element.likes;
         dislikesDisplay.textContent=element.Dislikes;
        
@@ -45,18 +49,36 @@ function renderList(characterObj){
         
     })
 }
-     addLike.addEventListener("click",(event)=>{
-    let counter = 0;
-    counter =counter +1;
-     
-    likesDisplay.textContent=counter;
-    patchRequest(objGlobal)  
+         addLike.addEventListener("click",(event)=>{
+          counter =counter +1;
+       
+           likesDisplay.textContent=counter;
+           patchRequest(objGlobal)  
     })
-    addDislike.addEventListener("click",(event)=>{
-        let counter = 0;
-        counter =counter +1;
+            addDislike.addEventListener("click",(event)=>{
+        
+        counter =counter+1;
         dislikesDisplay.textContent=counter;
         patchRequest(objGlobal)  
         })
     
+    const form =document.getElementById("quote-form");
+
+    form.addEventListener("submit",function(e){
+        e.preventDefault();
+
+        const prePayload =new FormData(form);
+        const payload =new URLSearchParams(prePayload);
+
+        console.log([...payload]);
+
+        fetch("http://localhost:3000/quotes",{
+            method: "POST",
+            body:payload,
+        })
+        .then(response =>response.json())
+        .then(data =>console.log(data))
+        .catch(error =>console.error(error))
+        patchRequest(objGlobal)  
+    })
 
